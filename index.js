a6a4a508673dacb682c906e9698a5ae1092c4a44
mhrayhan <mhrayhan430@gmial.com>
2022-05-18 15:05:08 +0600

    booking api add

--- index.js
+++ index.js
@@ -19,13 +19,36 @@ async function run(){
     try{
         await client.connect();
         const serviceCollection = client.db('docotors_portal').collection('services');
+        const bookingCollection = client.db('docotors_portal').collection('booking');
         console.log('connected');
+
+
 
         app.get('/service', async(req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        /*
+        * API naming convention
+        * app.get('/booking') // get all booking in this collection. or get more than one or by filter.
+        * app.get('/booking/:id') // get a specific booking
+        * app.post('/booking') // add a new booking
+        * app.patch('/booking/:id') // update
+        * app.delete('/booking/:id') // delete
+        */
+
+        app.post('/booking', async (req, res) => {
+           const booking = req.body;
+           const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient}
+           const exist = await bookingCollection.findOne(query)
+           if(exist){
+             return res.send({success: false, booking: exist})
+           }
+           const result = await bookingCollection.insertOne(booking);
+           return res.send({ success: true , result });
         })
 
     }
